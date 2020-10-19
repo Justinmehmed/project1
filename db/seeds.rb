db0ce3379f26ddc57aaf511688d5bea9c209c916
justinmehmed <justin.mehmed@yahoo.com>
2020-10-19 22:09:04 +1100

    again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,24 @@
 Book.destroy_all
 
-
-Book.create :title => 'The Name of the Wind: The Kingkiller Chronicle, Book 1', :author => "Patrick Ruffus", :description => ""
-
+Book.create(:title => "The Name of the Wind: The Kingkiller Chronicle, Book 1", :author => 'Patrick Ruffus', :description => '')
+Book.create(:title => "The Wise Man's Fear: The Kingkiller Chronicle: Book 2", :author => "Patrick Ruffus", :description => "")
 
 puts "#{ Book.count}"
+
+
+Category.destroy_all
+
+Category.create(:name => 'Fantasy')
+Category.create(:name => 'Sci fi')
+Category.create(:name => 'Horror')
+Category.create(:name => 'True Crime')
+Category.create(:name => 'Thriller')
+Category.create(:name => "Children's")
+Category.create(:name => 'Travel')
+Category.create(:name => 'Art')
+Category.create(:name => 'History')
+Category.create(:name => 'Romance')
+Category.create(:name => 'Biography')
+puts "#{ Category.count}"
+
 
