edb6a410c4e41dc6f01ce9ce778ac3775d90f389
justinmehmed <justin.mehmed@yahoo.com>
2020-10-22 20:20:41 +1100

    oh boy

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2020_10_20_104156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
